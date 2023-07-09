7409b7b56dddaa9e77231a5c2f27e72a9c22df6a
Corentin Gouil <gouilcorentin@gmail.com>
2023-07-09 15:23:27 +0200

    feat: create resp reply

---
+++ src/resp_reply/RespReply.h
@@ -0,0 +1,14 @@
+//
+// Created by gouil on 09/07/2023.
+//
+
+#ifndef CPP_REDIS_LIKE_RESPREPLY_H
+#define CPP_REDIS_LIKE_RESPREPLY_H
+
+
+class RespReply {
+
+};
+
+
+#endif //CPP_REDIS_LIKE_RESPREPLY_H
